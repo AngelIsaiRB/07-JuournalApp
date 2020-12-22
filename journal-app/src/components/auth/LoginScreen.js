55503cc208a367339094c9ec65f50ef24c6272f9
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-21 22:01:03 -0600

    view create count scss

--- journal-app/src/components/auth/LoginScreen.js
+++ journal-app/src/components/auth/LoginScreen.js
@@ -1,17 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const LoginScreen = () => {
     return (
         <div>
-            <h3>Login</h3>
+            <h3 className="auth__title">Login</h3>
             <form>
-                <input type="text" placeholder="email" name="email"></input>
-                <input type="password" placeholder="password" name="password"></input>
+                <input className="auth__input" autocomplete="off" type="text" placeholder="Email" name="email"></input>
+                <input className="auth__input" type="password" placeholder="password" name="Password"></input>
 
-                <button type="submit" >Login</button>
+                <button className="btn btn-primary btn-block" type="submit" >Login</button>
 
                 <hr/>
-                google login
+                <div className="auth__social-networks">
+                    <p>login whit social network</p>
+                    <div className="google-btn">
+                        <div className="google-icon-wrapper">
+                            <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+                        </div>
+                        <p className="btn-text">
+                        <b>Sign in with google</b>
+                        </p>
+                    </div>
+                </div>
+                <Link  to="/auth/register">
+                create new account
+                </Link>
             </form>
         </div>
     )
