dffe3f41ee38fd99d1cca8e1671f596e19ec2f5c
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-22 23:07:17 -0600

    home Screen desing

--- journal-app/src/components/notes/NoteScreen.js
+++ journal-app/src/components/notes/NoteScreen.js
@@ -5,6 +5,31 @@ export const NoteScreen = () => {
     return (
         <div className="notes__main-content"> 
             <NotesAppBar/>
+
+            <div className="notes__content">
+                <input
+                    className="notes__title-input"
+                    type="text"
+                    placeholder="some awesome title"
+                    autocomplete="off"
+                ></input>
+                <textarea
+                    className="notes__textarea"
+                    placeholder="what happened today?"
+                ></textarea>
+                <div className="notes__image">
+                    <img
+                        src="https://miro.medium.com/max/3200/1*qyhua3giJFAx_kOdFoWHzw.jpeg"
+                        alt="imagen"
+                    >
+                    </img>
+
+                </div>
+
+
+            </div>
+
+
         </div>
     )
 }
