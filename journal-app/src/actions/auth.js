b1028e81b748cd163148750b86b864d4a9d14ea2
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-23 17:51:11 -0600

    asyncs functions example redux

--- journal-app/src/actions/auth.js
+++ journal-app/src/actions/auth.js
@@ -1,6 +1,13 @@
 
 import { types } from "../types/types"
 
+export const startLoginEmailPassword = ()=>{
+    return (dispatch) =>{
+        setTimeout(() =>{
+            dispatch(login(1234,"isai"));
+        },3500)
+    }
+}
 
 export const login =(uid, displayName)=>(
      {
