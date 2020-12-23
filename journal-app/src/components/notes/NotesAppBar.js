8dd499744a49fc1110a728448e99df0c85660fd6
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-22 21:35:40 -0600

    Note appbar

---
+++ journal-app/src/components/notes/NotesAppBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const NotesAppBar = () => {
+    return (
+        <div className="notes__appbar"> 
+            <span>28 de algo del alfo</span>
+            <div>
+                <button className="btn">
+                    picture
+                </button>
+                <button className="btn">
+                    Save
+                </button>
+            </div>
+        </div>
+    )
+}
