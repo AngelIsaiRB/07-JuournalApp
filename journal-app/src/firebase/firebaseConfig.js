63e28aa6cf1278da1bb6b372a322ff0e003c20c0
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-28 21:31:04 -0600

    generar env

--- journal-app/src/firebase/firebaseConfig.js
+++ journal-app/src/firebase/firebaseConfig.js
@@ -4,35 +4,26 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const  firebaseConfig = {
-    apiKey: "AIzaSyDau3TE2iI9nVnZktCUQ07bcX7Ulmf1AGQ",
-    authDomain: "react-apps-143cc.firebaseapp.com",
-    projectId: "react-apps-143cc",
-    storageBucket: "react-apps-143cc.appspot.com",
-    messagingSenderId: "868512409642",
-    appId: "1:868512409642:web:a9e1979689b0cd6d857b53",
-    measurementId: "G-01QK4JS8D1"
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASEURL,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID,
   };
 
-  const firebaseConfigTesting = {
-    apiKey: "AIzaSyCv1lzdzAPWBhQ3nTqPWrDe7C0pjKa0uRI",
-    authDomain: "pushnotifications-cd953.firebaseapp.com",
-    databaseURL: "https://pushnotifications-cd953.firebaseio.com",
-    projectId: "pushnotifications-cd953",
-    storageBucket: "pushnotifications-cd953.appspot.com",
-    messagingSenderId: "731203614350",
-    appId: "1:731203614350:web:40e0ff0b1184fe28503bdf",
-    measurementId: "G-FBYGCHP854"
-  };
-
-  if(process.env.NODE_ENV === 'test'){
-    // testing firebaseConfigTesting
-    firebase.initializeApp(firebaseConfigTesting);
-  }
-  else{
-    //productions or delevopmen
-    firebase.initializeApp(firebaseConfig);
-  }
+  // if(process.env.NODE_ENV === 'test'){
+  //   // testing firebaseConfigTesting
+  //   firebase.initializeApp(firebaseConfigTesting);
+  // }
+  // else{
+  //   //productions or delevopmen
+  //   firebase.initializeApp(firebaseConfig);
+  // }
   
+  firebase.initializeApp(firebaseConfig);
 
   const db = firebase.firestore();
   const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
